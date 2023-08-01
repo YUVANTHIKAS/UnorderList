bcbd7446f6e82c4f34e1444420e30acd9d2aa9fc
Yuvanthika Sarathy <yuvanthika.s@tvsnext.com>
2023-08-01 21:52:35 +0530

    [yuvanthika] Add:Created

---
+++ UnorderList/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnorderList
+{
+    
+    
+        public class Node<T>//int,string,char,
+        {
+            public T data;
+            public Node<T> next;
+            public Node(T data)
+            {
+                this.data = data;
+            }
+        }
+    
+}
---
+++ UnorderList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using UnorderedList.UnorderedList;
+using UnorderedList.UnorderedListQueue;
+using UnorderedList.UnorderedListStack;
+
+namespace UnorderedList
+{
+    class Program
+    {
+        static string unOrderedFilePath = @"D:\BridgeLabs Training\UnorderedList\UnorderedList\UnorderedList\UnOrderList.txt";
+        static void Main(string[] args)
+        {
+            Operation operation = new Operation();
+            operation.ReadFileAndPerformOperation(unOrderedFilePath);
+            Console.WriteLine("\nStack:");
+            OperationStack operationStack = new OperationStack();
+            operationStack.ReadFileAndPerformOperation(unOrderedFilePath);
+            Console.WriteLine("\nQueue:");
+            OperationQueue operationqueue = new OperationQueue();
+            operationqueue.ReadFileAndPerformOperation(unOrderedFilePath);
+
+        }
+    }
+}
